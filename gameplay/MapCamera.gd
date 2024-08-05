81c9e30e94a7cac70a894e322a660992ea0db4f6
Konstantin Klepikov <oformleno@gmail.com>
2024-08-06 02:36:11 +0300

    points fixes

--- gameplay/MapCamera.gd
+++ gameplay/MapCamera.gd
@@ -4,7 +4,6 @@ var zoom_target: Vector2
 var mouse_start_position = Vector2.ZERO
 var camera_start_position = Vector2.ZERO
 var is_draging: bool = false
-var path_node = preload("res://points/path_node.tscn")
 
 
 func _ready():
@@ -14,8 +13,7 @@ func _ready():
 func _process(delta):
 	make_zoom(delta)
 	camera_drag()
-	add_path_node()
-	
+
 
 func make_zoom(delta) -> void:
 	if Input.is_action_just_pressed("zoom_in"):
@@ -37,14 +35,3 @@ func camera_drag():
 	if is_draging:
 		var move_vector = get_viewport().get_mouse_position() - mouse_start_position
 		position = camera_start_position - move_vector * 1/zoom.x
-		
-		
-func add_path_node():
-	if (
-		Input.is_action_just_pressed("add_node") 
-		and get_tree().get_current_scene().get_name() == 'Strategic'
-		and get_parent().in_rect == true
-	):
-		var pn = path_node.instantiate()
-		pn.position = to_local(get_global_mouse_position())
-		add_child(pn)
--- gameplay/gameplay.gd
+++ gameplay/gameplay.gd
@@ -1,10 +1,11 @@
 extends Node2D
 
 var in_rect: bool = true
+var path_node = preload("res://points/path_node.tscn")
 
 
 func _process(_delta: float) -> void:
-	pass
+	add_path_node()
 
 
 func _on_texture_rect_mouse_entered():
@@ -13,3 +14,14 @@ func _on_texture_rect_mouse_entered():
 
 func _on_texture_rect_mouse_exited():
 	in_rect = false
+
+
+func add_path_node():
+	if (
+		Input.is_action_just_pressed("add_node") 
+		and get_tree().get_current_scene().get_name() == 'Strategic'
+		and in_rect == true
+	):
+		var pn = path_node.instantiate()
+		pn.position = to_local(get_global_mouse_position())
+		add_child(pn)
